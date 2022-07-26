c79ccfc2b1e21456e93bfecdf38af87dfacc8ee1
JorgeOlanSantos <89498253+JorgeOlanSantos@users.noreply.github.com>
2022-07-26 06:19:13 -0700

    Created UserTest [incomplete]
    Created test case for User.java [incomplete]

---
+++ src/testBankingSystem/UserTest.java
@@ -0,0 +1,45 @@
+//package testBankingSystem; // Package all test cases together?
+
+import static org.junit.jupiter.api.Assertions.*; // To use assertions
+import org.junit.jupiter.api.Test;                // To code @Test methods
+
+class UserTest { // Test Customer class
+	// Test input
+	String testName = "Name";
+	String testID = "ID";
+	String testPassword = "Password";
+	
+	@Test
+	void testConstructorName() {
+		// Create new User object with test input
+		User user = new User(testName, testID, testPassword);
+		
+		assertEquals(testName, user.getName()); // Check name
+	}
+	
+	@Test
+	void testConstructorLoginID() {
+		// Create new User object with test input
+		User user = new User(testName, testID, testPassword);
+		
+		assertEquals(testID, user.getID()); // Check ID
+	}
+	
+	@Test
+	void testConstructorLoginPassword() {
+		// Create new User object with test input
+		User user = new User(testName, testID, testPassword);
+		
+		assertEquals(testPassword, user.getPassword()); // Check password
+	}
+	
+	@Test
+	void testConstructorLogintoString() {
+		// Create new User object with test input
+		User user = new User(testName, testID, testPassword);
+		
+		String LoginString = "ID: " + testID + ", password: " + testPassword;
+		
+		assertEquals(LoginString, user.getLoginString()); // Check password
+	}
+}
