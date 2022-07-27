eb8f242bd6379abdcded9c47b556732f4e258648
JorgeOlanSantos <89498253+JorgeOlanSantos@users.noreply.github.com>
2022-07-27 07:22:30 -0700

    Added AccountTest [incomplete]
    AccountTest current just has empty test methods with the generic fail(); response for now

---
+++ src/testBankingSystem/AccountTest.java
@@ -0,0 +1,48 @@
+package testBankingSystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+	@Test
+	void testGetAccountID() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetBalance() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetHistory() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetCustomerIDS() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testDeposit() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testWithdraw() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testAddCustomerID() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testRemoveCustomer() {
+		fail("Not yet implemented");
+	}
+}
